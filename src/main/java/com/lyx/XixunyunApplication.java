bb5cf05b55525617681d5cf63d4a66151831c44e
2414690715 <59909125+2414690715@users.noreply.github.com>
2022-05-12 22:24:32 +0800

    Update XixunyunApplication.java

--- src/main/java/com/lyx/XixunyunApplication.java
+++ src/main/java/com/lyx/XixunyunApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableScheduling
 public class XixunyunApplication {
 
     public static void main(String[] args) {
